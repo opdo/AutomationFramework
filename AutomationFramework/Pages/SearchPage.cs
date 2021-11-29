40ade83c6d0a9849a3edf477a70bb2c0c85c43c9
ADMIN <ADMIN@VINH>
2021-11-29 15:04:38 +0700

    Add project files.

---
+++ AutomationFramework/Pages/SearchPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework.Pages
+{
+    public class SearchPage
+    {
+        private IWebDriver driver;
+
+        public SearchPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // Element
+        public IWebElement InputSearch => driver.FindElement(By.XPath("//input[@data-bind = 'textInput: studentID']"));
+        public IWebElement ButtonSearch => driver.FindElement(By.XPath("//button[@id = 'btnSearch']"));
+
+        // Method
+        public void SearchByStudentCode(string code)
+        {
+            InputSearch.SendKeys(code);
+            ButtonSearch.Click();
+        }
+    }
+}
---
+++ AutomationFramework/Tests/SearchTest.cs
@@ -0,0 +1,31 @@
+﻿using AutomationFramework.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutomationFramework.Tests
+{
+    public class SearchTest
+    {
+        public IWebDriver Driver;
+        readonly SearchPage searchPage;
+
+        public SearchTest()
+        {
+            Driver = new ChromeDriver();
+            searchPage = new SearchPage(Driver);
+        }
+
+        [Fact]
+        public void SearchCorrectStudentCode()
+        {
+            Driver.Url = "https://hutechcheckin.com/";
+            searchPage.SearchByStudentCode("1611060042");
+        }
+    }
+}
